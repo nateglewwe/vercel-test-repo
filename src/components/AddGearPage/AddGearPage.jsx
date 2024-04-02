7183dcfde5dee60e1ea3c5fd0a05ce7e7a385afc
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-04-02 10:07:21 -0500

    Made form for AddGearPage

--- src/components/AddGearPage/AddGearPage.jsx
+++ src/components/AddGearPage/AddGearPage.jsx
@@ -71,26 +71,53 @@ function AddGearPage(props) {
 
   return (
     <div>
-      <h1>Add Gear:</h1>
-      {imagePreview ? 
-        <img src={imagePreview} alt={`Photo of ${fileName}`} />
-      :
-        <p>Add a photo to display preview here</p>
-      }<br />
-      <input type="file" accept="image/*" id="fileUploader"onChange={onFileChange} />
-      <input type="button" value="Delete Photo" onClick={() => deletePhoto()}/><br />
-      <h4>Name:</h4>
-      <input id="nameInput" placeholder="Name" value={nameInput}
-          onChange={(event) => {setNameInput(event.target.value)}} />
-      <h4>Features:</h4>
-      <input id="feature1Input" placeholder="Feature 1" value={feature1Input}
-          onChange={(event) => {setFeature1Input(event.target.value)}} /><br />
-
       <form onSubmit={addGearToList}>
-        <FeatureInput /><br />
-        <FeatureInput /><br />
-        <FeatureInput /><br />
-      <button type="submit">Add Gear</button>
+        <h1>Add Gear:</h1>
+        {imagePreview ? 
+          <img src={imagePreview} alt={`Photo of ${fileName}`} />
+        :
+          <p>Add a photo to display preview here</p>
+        }<br />
+        <input type="file" accept="image/*" id="fileUploader"onChange={onFileChange} />
+        <input type="button" value="Delete Photo" onClick={() => deletePhoto()}/><br />
+        <h4>Name:</h4>
+        <input id="nameInput" placeholder="Name (Required)" value={nameInput}
+            onChange={(event) => {setNameInput(event.target.value)}} required/>
+        <h4>Features:</h4>
+        <input id="feature1Input" placeholder="Feature 1" value={feature1Input}
+            onChange={(event) => {setFeature1Input(event.target.value)}} /><br />
+        <input id="feature2Input" placeholder="Feature 2" value={feature2Input}
+            onChange={(event) => {setFeature2Input(event.target.value)}} /><br />
+        <input id="feature3Input" placeholder="Feature 3" value={feature3Input}
+            onChange={(event) => {setFeature3Input(event.target.value)}} /><br />
+        <input id="feature4Input" placeholder="Feature 4" value={feature4Input}
+            onChange={(event) => {setFeature4Input(event.target.value)}} /><br />
+        <input id="feature5Input" placeholder="Feature 5" value={feature5Input}
+            onChange={(event) => {setFeature5Input(event.target.value)}} /><br />
+        <input id="feature6Input" placeholder="Feature 6" value={feature6Input}
+            onChange={(event) => {setFeature6Input(event.target.value)}} /><br />
+        <input id="feature7Input" placeholder="Feature 7" value={feature7Input}
+            onChange={(event) => {setFeature7Input(event.target.value)}} /><br />
+        <input id="feature8Input" placeholder="Feature 8" value={feature8Input}
+            onChange={(event) => {setFeature8Input(event.target.value)}} /><br />
+        <h4>Notes:</h4>
+        <input id="note1Input" placeholder="Note 1" value={note1Input}
+            onChange={(event) => {setNote1Input(event.target.value)}} /><br />
+        <input id="note2Input" placeholder="Note 2" value={note2Input}
+            onChange={(event) => {setNote2Input(event.target.value)}} /><br />
+        <input id="note3Input" placeholder="Note 3" value={note3Input}
+            onChange={(event) => {setNote3Input(event.target.value)}} /><br />
+        <input id="note4Input" placeholder="Note 4" value={note4Input}
+            onChange={(event) => {setNote4Input(event.target.value)}} /><br />
+        <input id="note5Input" placeholder="Note 5" value={note5Input}
+            onChange={(event) => {setNote5Input(event.target.value)}} /><br />
+        <input id="note6Input" placeholder="Note 6" value={note6Input}
+            onChange={(event) => {setNote6Input(event.target.value)}} /><br />
+        <input id="note7Input" placeholder="Note 7" value={note7Input}
+            onChange={(event) => {setNote7Input(event.target.value)}} /><br />
+        <input id="note8Input" placeholder="Note 8" value={note8Input}
+            onChange={(event) => {setNote8Input(event.target.value)}} /><br />
+        <button type="submit">Add Gear</button>
       </form>
       
 
@@ -100,16 +127,53 @@ function AddGearPage(props) {
 
 export default AddGearPage;
 
-const updateKeyValue = (key, newValue) => {
-  setState(prevState => ({
-    ...prevState,
-    [key]: newValue
-  }));
-};
+// const updateKeyValue = (key, newValue) => {
+//   setState(prevState => ({
+//     ...prevState,
+//     [key]: newValue
+//   }));
+// };
 
 const changeFeature = (featureNum, newValue) => {
-  setFeatures(prevState => ({
-    ...prevState,
-    featureNum: newValue
-  }))
+  if (featureNum === 1) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature1: newValue
+    }));
+  } else if (featureNum === 2) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature2: newValue
+    }));
+  } else if (featureNum === 3) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature3: newValue
+    }));
+  } else if (featureNum === 4) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature4: newValue
+    }));
+  } else if (featureNum === 5) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature5: newValue
+    }));
+  } else if (featureNum === 6) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature6: newValue
+    }));
+  } else if (featureNum === 7) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature7: newValue
+    }));
+  } else if (featureNum === 8) {
+    setFeatures(prevState => ({
+      ...prevState,
+      feature8: newValue
+    }));
+  } 
 }
