afc6fa7906265f340145ff905a418b9ff6859726
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-04-06 22:47:30 -0500

    Changing CSS of Nav Bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,10 +9,9 @@ function Nav() {
 
   return (
     <div className="nav">
-      <Link to="/home">
+      {/* <Link to="/home">
         <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
+      </Link> */}
         {/* If no user is logged in, show these links */}
         {!user.id && (
           // If there's no user, show login/registration links
@@ -24,21 +23,11 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
+            {/* <Link className="navLink" to="/user">
               Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
+            </Link> */}
           </>
         )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
 
         <Link className="navLink" to="/gearlist">
           Gear
@@ -55,7 +44,8 @@ function Nav() {
         <Link className="navLink" to="/addevent">
           Add Event
         </Link>
-      </div>
+
+        <LogOutButton className="navLink" />
     </div>
   );
 }
