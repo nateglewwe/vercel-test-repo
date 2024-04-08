fc9c4edf436b6ed34b39a3d1a003d7733d07015d
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-04-07 23:46:12 -0500

    Trying to figure out how to store a previous state

--- src/components/GearListPage/GearListPage.jsx
+++ src/components/GearListPage/GearListPage.jsx
@@ -34,14 +34,15 @@ function GearListPage(props) {
     dispatch({ type: 'DELETE_GEAR', payload: toolId });
   }
 
-  const assignToEvent = (toolId, eventId) => {
-    //Dispatching event assignement for piece of gear to gear_list table in DB
-    if (eventId === '') {
-      dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: null}});
-    } else {
-      dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: eventId}});
-    }
-  };
+  // const assignToEvent = (toolId, eventId) => {
+  //   //Dispatching event assignement for piece of gear to gear_list table in DB
+  //   if (eventId === '') {
+  //     dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: null}});
+  //     dispatch({ type: 'ADD_TO_EVENT_GEARLIST', payload: {id: null, eventId: null}});
+  //   } else {
+  //     dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: eventId}});
+  //   }
+  // };
 
   return (
     <div className="gearListDOM" >
@@ -51,6 +52,22 @@ function GearListPage(props) {
                           tool.feature_5, tool.feature_6, tool.feature_7, tool.feature_8];
         const notes = [tool.note_1, tool.note_2, tool.note_3, tool.note_4,
                        tool.note_5, tool.note_6, tool.note_7, tool.note_8];
+        const [prevEventId, setPrevEventId] = useState(tool.event_id || '');
+
+        const assignToEvent = (toolId, eventId) => {
+          console.log('PREVIOUS INTERATION OF tool.event_id:', prevEventId);
+          //Dispatching event assignement for piece of gear to gear_list table in DB
+          //ALSO dispatching gear assignment to event_list table in DB
+          if (eventId === '') {
+            dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: null}});
+            // dispatch({ type: 'ADD_TO_EVENT_GEARLIST', payload: {id: null, eventId: prevEventId}});
+          } else {
+            dispatch({ type: 'ASSIGN_TO_EVENT', payload: {id: toolId, eventId: eventId}});
+          }
+          setPrevEventId(tool.event_id || '')
+          console.log('NEXT INTERATION OF tool.event_id:', prevEventId);
+        };
+
           return (
             <div key={tool.id}>
               <Grid container spacing={2} className="gearGridList">
