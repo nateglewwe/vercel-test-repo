7a271b400cb7d10f365538155f47e1d786a716dc
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-04-26 18:24:36 -0500

    new ver of gearAssignEvent is working as intended now

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -195,37 +195,66 @@ router.put('/gearChangeNote/:id', (req, res) => {
 } else {console.log('DID NOT PASS THE CHANGENOTE SERVER ROUTER WHITELIST CHECK'); res.json({alert: 'BAD ACTOR OH NO DO SOMETHING'})}
 });
 
-// UPDATE "event_list" SET gear_1_id = null WHERE gear_1_id = $1;
-// UPDATE "event_list" SET gear_2_id = null WHERE gear_2_id = $1;
-// UPDATE "event_list" SET gear_3_id = null WHERE gear_3_id = $1;
-// UPDATE "event_list" SET gear_4_id = null WHERE gear_4_id = $1;
-// UPDATE "event_list" SET gear_5_id = null WHERE gear_5_id = $1;
-// UPDATE "event_list" SET gear_6_id = null WHERE gear_6_id = $1;
-// UPDATE "event_list" SET gear_7_id = null WHERE gear_7_id = $1;
-// UPDATE "event_list" SET gear_8_id = null WHERE gear_8_id = $1;
-
-// UPDATE "event_list"
-// SET gear_1_id = $1
-// WHERE "event_list".id = $2;
-
-// UPDATE "gear_list"
-// SET event_id = null
-// WHERE event_id = $2;
-
 router.put('/gearAssignEvent/:id', (req, res) => {
-  const queryText = `
+  const nullOutGear1Query = `UPDATE "event_list" SET gear_1_id = null WHERE gear_1_id = $1;`;
+  // const nullOutGear2Query = `UPDATE "event_list" SET gear_2_id = null WHERE gear_2_id = $1;`;
+  // const nullOutGear3Query = `UPDATE "event_list" SET gear_3_id = null WHERE gear_3_id = $1;`;
+  // const nullOutGear4Query = `UPDATE "event_list" SET gear_4_id = null WHERE gear_4_id = $1;`;
+  // const nullOutGear5Query = `UPDATE "event_list" SET gear_5_id = null WHERE gear_5_id = $1;`;
+  // const nullOutGear6Query = `UPDATE "event_list" SET gear_6_id = null WHERE gear_6_id = $1;`;
+  // const nullOutGear7Query = `UPDATE "event_list" SET gear_7_id = null WHERE gear_7_id = $1;`;
+  // const nullOutGear8Query = `UPDATE "event_list" SET gear_8_id = null WHERE gear_8_id = $1;`;
+  const nullOutGear1QueryArg = [req.params.id]
+
+  const setGearForEventQuery = `
+  UPDATE "event_list"
+  SET gear_1_id = $1
+  WHERE "event_list".id = $2;
+  `;
+  const setGearForEventQueryArgs = [req.params.id, req.body.eventId]
+
+  const nullOutPrevEventQuery = `
+  UPDATE "gear_list"
+  SET event_id = null
+  WHERE event_id = $1;
+  `;
+  const nullOutPrevEventQueryArg = [req.body.eventId]
+
+  const assignEventQuery = `
   UPDATE "gear_list"
   SET event_id = $2
   WHERE "gear_list".id = $1;
   `;
-  const queryArgs = [req.params.id, req.body.eventId]
-  pool.query(queryText, queryArgs)
+  const assignEventQueryArgs = [req.params.id, req.body.eventId]
+
+  pool.query(nullOutGear1Query, nullOutGear1QueryArg)
   .then(result => {
-    console.log('Gear with following ID has had event_id changed to:', req.params.id, req.body.eventId);
-    res.sendStatus(200);
+    pool.query(setGearForEventQuery, setGearForEventQueryArgs)
+    .then (result => {
+      pool.query(nullOutPrevEventQuery, nullOutPrevEventQueryArg)
+      .then (result => {
+        pool.query(assignEventQuery, assignEventQueryArgs)
+        .then(result => {
+          console.log('Gear with following ID has had event_id changed to:', req.params.id, req.body.eventId);
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          console.log('ERROR in assignEventQuery in server route:', err);
+          res.sendStatus(500);
+        });
+      })
+      .catch((err) => {
+        console.log('ERROR in nullOutPrevEventQuery in server route:', err);
+        res.sendStatus(500);
+      });
+    })
+    .catch((err) => {
+      console.log('ERROR in setGearForEventQuery in server route:', err);
+      res.sendStatus(500);
+    });
   })
   .catch((err) => {
-    console.log('ERROR in assigning gear to event server route:', err);
+    console.log('ERROR in nullOutGear1Query in server route:', err);
     res.sendStatus(500);
   });
 });
